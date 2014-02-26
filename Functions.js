8d0e91222eae9b9b612584ad2fc149410f776d2f
Tim Binney <tjbinney@fullsail.edu>
2014-02-26 17:52:29 -0500

    Added Decimal Rounding Function

--- Functions.js
+++ Functions.js
@@ -4,14 +4,22 @@
 // Function Calls
 // Term 1402
 
-
+// Global Variables
 var emailInfo;
-var emailConfirmation;
 var phoneReturn;
 var myPhone = "123-456-7890";
-var urlValidation;
 var urlReturn;
 var myUrl = "https://gmail.com";
+var numberString = "239";
+var stringToNumber;
+var myArray = [1,2,3,4,5,6,7,8,9,10];
+var arrayFunction;
+var arrayFunctionCall;
+var lowerCaseString = "i wish i could fly!";
+var upperCaseFunction;
+var roundFunction;
+var myDecimal = 10.4567889;
+var fixedDecimal = 2;
 
 // String Phone Confirm
 var phoneReturn = function(phoneNumber) {
@@ -73,7 +81,98 @@ var urlValidation = urlReturn(myUrl);
     else
         console.log("not a url");
 
+// Number Parse
+var stringToNumber = function(numberParsed) {
+    var convertString = parseInt(numberParsed)
+    
+    return (convertString);    
 
+};
+
+var parsingString = stringToNumber(numberString);
+
+if (parsingString === 239) {
+    console.log("Oh ya we are parsing numbers!") 
+} else
+    console.log("Failed to parse the number!");
+    
+
+
+// Round Decimal Points Function
+var roundFunction = function(decimalUsed,decimalFixed){
+    var newNumber = decimalUsed;
+    var numberOfDecimals = decimalFixed;
+        newNumber = newNumber.toFixed(numberOfDecimals);
+    return newNumber;
+      
+}
+
+
+roundFunctionCall = roundFunction(myDecimal,fixedDecimal);
+
+console.log(roundFunctionCall);
+
+
+
+
+
+
+
+
+
+
+
+// Uppercase String
+
+
+var upperCaseFunction = function(stringUsed){
+    var newString = stringUsed.toUpperCase();
+    console.log(newString);
+    if (newString === "I Wish I Could Fly!") {
+        console.log("Uppercasing!")
+    }
+    else
+    console.log("Not doing what i'm suppose to!")
+    
+   return newString;
+}
+console.log(upperCaseFunction);
+
+var callUpperCaseFunction = upperCaseFunction(lowerCaseString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Greatest Value Array
+var arrayFunction = function(value, index, array){
+    var arrayReturn = false;
+    if (value > 3) {
+        arrayReturn = true;
+    }
+    return arrayReturn;
+    
+    if (myArray.some(arrayFunction)) {
+        var arrayResult = myArray.filter(arrayFunction);
+        alert("these are greater then 3: " + arrayResult);
+    }
+};
+    
+
+var arrayFunctionCall = arrayFunction(value1, index , myArray);
 
 
 
